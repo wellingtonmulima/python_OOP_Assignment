afed4828b8d319f9f46ce28f5f764d17c90bf748
codedark1 <wellingtonmulima@gmail.com>
2024-03-02 01:28:40 +0300

    my_list assignment

---
+++ my_list.py
@@ -0,0 +1,28 @@
+my_list=[] #creating an empty list
+my_list.append(10) #adding a value to the empty list
+my_list.append(20)#adding a value to the empty list
+my_list.append(30)#adding a value to the empty list
+
+my_list.append(40
+               )#adding a value to the empty list
+print(my_list)
+
+my_list.insert(1,15)#inserting a new value to the list
+
+print(my_list)
+
+my_list2=[50,60,70]#creating new list list_2
+
+my_list.extend(my_list2)#combining the two lists
+
+print(my_list)
+
+my_list.remove(70)#removing a value from the list
+
+print(my_list)
+
+my_list.sort()#sorting the list
+
+print(my_list)
+
+print(my_list.index(30))#getting the index of a value on the list
